58e966c6076ddb9220e0cb84b637542b700944d6
Johan van Eck <jve.software@gmail.com>
2025-12-03 09:16:13 +0200

    Fix naming & add day 4 files

---
+++ Day-04/Day-04.cs
@@ -0,0 +1,30 @@
+namespace Aoc2025;
+
+public static class Day04
+{
+    public static void Run()
+    {
+        // Read file into string
+        var input = File.ReadAllText("Day-03/input.txt");
+
+        // Run part 1
+        var result = Part01(input);
+
+        // Run part 2
+        var result2 = Part02(input);
+
+        // Print result
+        Console.WriteLine(result);
+        Console.WriteLine(result2);
+    }
+
+    public static long Part01(string input)
+    {
+        return 0;
+    }
+
+    public static long Part02(string input)
+    {
+        return 0;
+    }
+}
